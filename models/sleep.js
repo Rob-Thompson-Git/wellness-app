d788a20a411b8603796b1048a5c7698be219dd2b
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-13 14:44:42 -0400

    added sleep.js to models

--- models/sleep.js
+++ models/sleep.js
@@ -3,28 +3,31 @@ const sequelize = require('../config/connection');
 
 class Sleep extends Model {}
 
-Sleep.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  hours: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-  },
-  quality: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  {
-    sequelize, 
-    timestamps: false, 
-    freezeTableName: true,
-    underscored: true,
-    modelName: "sleep",
-  }
-});
+  Sleep.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      hours: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      quality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'sleep',
+    }
+  );
+  
+  module.exports = Sleep;
+  
 
-module.exports = Sleep;
