7df557e8105b4f7d4584e76abb45c2f6e549c52b
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-13 14:14:38 -0400

    added to sleep.js

--- models/sleep.js
+++ models/sleep.js
@@ -1,1 +1,30 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Sleep extends Model {}
+
+Sleep.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  hours: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  quality: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  {
+    sequelize, 
+    timestamps: false, 
+    freezeTableName: true,
+    underscored: true,
+    modelName: "sleep",
+  }
+});
+
+module.exports = Sleep;
