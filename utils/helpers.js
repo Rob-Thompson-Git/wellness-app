fa31f76fec375814ae926f1dce82d546f78c9081
RobThompson <rob.thomp15@gmail.com>
2022-10-13 19:12:00 -0500

    added set foreign key checks to be able to seed user table data

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,5 @@
-module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
-  };
+// module.exports = {
+//     format_time: (date) => {
+//       return date.toLocaleTimeString();
+//     },
+//   };
