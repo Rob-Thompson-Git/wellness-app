b60cce1fe37c9504295e58669f80b941d28b7b8f
Yasmin <bustamiyasmin@gmail.com>
2022-10-17 14:28:18 -0500

    css wprk with quote/dt js split with Paul-TA

--- public/js/quote.js
+++ public/js/quote.js
@@ -12,35 +12,3 @@ function getQuote() {
 
 getQuote();
 
-//date and time funciton
-const timeEl = document.querySelector('#time');
-const dateEl = document.querySelector('#date');
-
-/**
- * @param {Date} date
- */
-function formatTime(date) {
-    const hrConverter = date.getHours() % 12 || 12;
-    const minutes = date.getMinutes();
-    const isAm = date.getHours() < 12;
-
-    return `${hrConverter.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
-}
-
-/**
- * @param {Date} date
- */
-function formatDate(date) {
-    const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const MONTHS = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December", ];
-
-    return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
-}
-
-setInterval(() => {
-    const current = new Date();
-
-    timeEl.textContent = formatTime(current);
-    dateEl.textContent = formatDate(current);
-}, 200);
-
