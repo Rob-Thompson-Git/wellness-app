f5755bbb40afab114289671d1575b6d676d56930
RobThompson <rob.thomp15@gmail.com>
2022-10-16 19:55:11 -0500

    stagin changes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,47 @@
-// const newFormHandler = async (event) => {
-//   event.preventDefault();
+const newFormHandler = async (event) => {
+  event.preventDefault();
 
-//   const sleep = document.querySelector('#sleep').value.trim();
-//   const mood = document.querySelector('#mood').value.trim();
-//   const activity = document.querySelector('#activity').value.trim();
+  const sleep = document.querySelector('#sleep').value.trim();
+  const mood = document.querySelector('#mood').value.trim();
+  const activity = document.querySelector('#activity').value.trim();
 
-//   if (sleep && mood && activity) {
-//     const response = await fetch(`/api/user`, {
-//       method: 'POST',
-//       body: JSON.stringify({sleep, mood, activity }),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
+  if (sleep && mood && activity) {
+    const response = await fetch(`/api/user`, {
+      method: 'POST',
+      body: JSON.stringify({sleep, mood, activity }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert('Failed to create project');
-//     }
-//   }
-// };
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create project');
+    }
+  }
+};
 
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
 
-//     const response = await fetch(`/api/user/${id}`, {
-//       method: 'DELETE',
-//     });
+    const response = await fetch(`/api/user/${id}`, {
+      method: 'DELETE',
+    });
 
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert('Failed to delete project');
-//     }
-//   }
-// };
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
 
-// document
-//   .querySelector('.new-user-form')
-//   .addEventListener('submit', newFormHandler);
+document
+  .querySelector('.new-user-form')
+  .addEventListener('submit', newFormHandler);
 
-// document
-//   .querySelector('.wellness')
-//   .addEventListener('click', delButtonHandler);
+document
+  .querySelector('.wellness')
+  .addEventListener('click', delButtonHandler);
